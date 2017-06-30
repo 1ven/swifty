8ce860c694efc4345479404dbd4b2b43ef5ecc22
Ivan Mironchik <me.roaiven@gmail.com>
2017-06-30 11:15:20 +0300

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { default as createModel } from "./createModel";
 export { default as createAction } from "./createAction";
 export { default as combineReducers } from "./combineReducers";
+
+export { Mapper, Reducer, ReducersSpec } from "./types";
