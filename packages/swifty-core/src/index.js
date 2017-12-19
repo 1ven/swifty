9e27cb8844eab66b16b2966600c55b836fb14aae
1ven <1ven.mironchik@gmail.com>
2017-12-19 21:57:01 +0300

    Implement createAction

--- packages/swifty-core/src/index.js
+++ packages/swifty-core/src/index.js
@@ -1,2 +1,19 @@
+import { Observable } from "rxjs";
+
 export const createStore = (reducer$, initialState) =>
   reducer$.scan((acc, fn) => fn(acc), initialState);
+
+export const createAction = () => {
+  let obs;
+
+  const stream$ = Observable.create(observer => {
+    obs = observer;
+  });
+
+  return {
+    stream$,
+    dispatch(payload) {
+      obs.next(payload);
+    }
+  };
+};
