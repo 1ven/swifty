b7e88b3a91efa5aa23d493ac0e7cc85194dfc10b
1ven <1ven.mironchik@gmail.com>
2017-12-19 21:59:46 +0300

    Change actions interface

--- packages/swifty-api/src/createStore.js
+++ packages/swifty-api/src/createStore.js
@@ -4,18 +4,18 @@ import { createStore } from "swifty-core";
 export default actions =>
   createStore(
     Observable.merge(
-      actions.request$.map(payload => state => ({
+      actions.request.stream$.map(payload => state => ({
         ...state,
         isFetching: true
       })),
-      actions.success$.map(payload => state => ({
+      actions.success.stream$.map(payload => state => ({
         ...state,
         isFetching: false,
         data: payload.body,
         error: void 0,
         lastUpdated: payload.meta.receivedAt
       })),
-      actions.failure$.map(payload => state => ({
+      actions.failure.stream$.map(payload => state => ({
         ...state,
         isFetching: false,
         error: payload.message
